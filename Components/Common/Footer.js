8f12decaa974301d5c69be90135e582bcadbd48a
valugit <44845228+valugit@users.noreply.github.com>
2020-01-20 14:02:43 +0100

    Structure update

---
+++ Components/Common/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Footer = () => <View>
+  <Text>© Supinternet 2020 - All rights reserved.</Text>
+</View>
+
+export default Footer;
