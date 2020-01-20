299e1d61124e64dcd628b1bd643e0faaf1c71e93
Anthony <anthonyd-m@live.com>
2020-01-20 15:16:17 +0100

    created auth header

--- Components/Authentication/Login.js
+++ Components/Authentication/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { AuthHeader } from '../Common/Headers';
 
 class Login extends React.Component {
     render() {
         const {navigate} = this.props.navigation;
         return (
             <View>
+                <AuthHeader/>
                 <Text>Login</Text>
                 <Button
                     title="Register"
---
+++ Components/Common/Headers.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+
+class AuthHeader extends React.Component {
+    render() { 
+        return (
+            <View style={styles.container}>
+                <View style={styles.leftCircle}></View>
+                <View style={styles.rightCircle}></View>
+                <Text style={styles.title}>{this.props.title || "Hello, connectez-vous à votre espace."}</Text>
+                <View style={styles.icon}>
+                    <Image source={""}/>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'relative',
+        height: 350,
+        display: "flex",
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    leftCircle: {
+        width: 280,
+        height: 280,
+        borderRadius: 140,
+        backgroundColor: "#000",
+        position: 'absolute',
+        top: -70,
+        left: -70,
+        zIndex: 5,
+    },
+    rightCircle: {
+        width: 350,
+        height: 350,
+        borderRadius: 200,
+        backgroundColor: "#333",
+        position: 'absolute',
+        top: -100,
+        right: -70,
+        zIndex: 1,
+    },
+    icon: {
+        width: 100,
+        height: 100,
+        shadowColor: '#FAFAFA',
+        shadowOpacity: 0.07,
+        shadowRadius: 50,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        zIndex: 10,
+        position: 'absolute',
+        top: 200,
+        backgroundColor: '#fff',
+        borderRadius: 50,
+    },
+    title: {
+        position: 'absolute',
+        top: 70,
+        width: 280,
+        color: "#fff",
+        fontSize: 23,
+        zIndex: 10,
+        textAlign: 'center',
+    }
+})
+ 
+export { AuthHeader };
